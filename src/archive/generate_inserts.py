9b61338d89023e9f86622bf95ae2bda288f86f23
mcho04 <minhyeogj231@gmail.com>
2026-03-12 14:17:07 -0700

    Seperated sentiment scores into three different columns (positive, neutral, negative)

---
+++ src/archive/generate_inserts.py
@@ -0,0 +1,54 @@
+from data_cleaning import basics, ratings, movie_sentiments
+
+import numpy as np
+import pandas as pd
+import re
+
+import pandas as pd
+
+def generate_inserts(df, table_name, columns, output_file):
+    with open(output_file, "a") as f:  # append mode
+        for _, row in df.iterrows():
+
+            # skip rows with missing values
+            if any(pd.isna(row[col]) for col in columns):
+                continue
+
+            values = []
+            for col in columns:
+                val = row[col]
+
+                if isinstance(val, str):
+                    val = val.replace("'", "''")  # escape quotes
+                    values.append(f"'{val}'")
+                else:
+                    values.append(str(val))
+ 
+            values_str = ", ".join(values)
+            cols_str = ", ".join(columns)
+
+            insert_stmt = f"INSERT INTO {table_name} ({cols_str}) VALUES ({values_str});\n"
+
+            f.write(insert_stmt)
+
+generate_inserts(
+    basics,
+    "movies",
+    ["tconst", "primaryTitle_norm", "startYear"],
+    "sql/insert_data.sql"
+)
+
+generate_inserts(
+    ratings,
+    "ratings",
+    ["tconst", "averageRating", "numVotes"],
+    "sql/insert_data.sql"
+)
+
+generate_inserts(
+    movie_sentiments,
+    "movie_sentiments",
+    ["name_norm", "trailer_link", "video_id", "sentiment_scores", 
+    "favorability", "rating", "genre", "year", "votes"],
+    "sql/insert_data.sql"
+)
