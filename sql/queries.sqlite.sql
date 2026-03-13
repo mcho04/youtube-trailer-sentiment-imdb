c13be6a69a70e641b20137203e595311fb92e4e7
mcho04 <minhyeogj231@gmail.com>
2026-03-13 13:22:03 -0700

    check

--- sql/queries.sqlite.sql
+++ sql/queries.sqlite.sql
@@ -1,41 +1,37 @@
--- .headers on
--- .mode column
+-- Observing tables
+SELECT * FROM movies LIMIT 1;
+SELECT * FROM ratings LIMIT 1;
+SELECT * FROM movie_sentiments LIMIT 1;
 
-SELECT COUNT(*) FROM movies;
+SELECT ms.genre, ms.favorability, r.averageRating
+FROM movies m 
+JOIN ratings r ON m.tconst = r.tconst
+JOIN movie_sentiments ms ON m.primaryTitle_norm = ms.name_norm
+WHERE ms.genre IN ('Action', 'Comedy');
 
-SELECT COUNT(*) FROM ratings;
+SELECT 
+    CASE 
+        WHEN r.averageRating >= 7 THEN 'High'
+        WHEN r.averageRating <= 4 THEN 'Low'
+    END AS rating_group,
+    ms.favorability,
+    r.averageRating
+FROM movies m
+JOIN ratings r ON m.tconst = r.tconst
+JOIN movie_sentiments ms ON m.primaryTitle_norm = ms.name_norm
+WHERE r.averageRating >= 7 OR r.averageRating <= 4
+ORDER BY rating_group;
 
-SELECT COUNT(*) FROM movie_sentiments;
-
--- SELECT ms.genre, ms.favorability, r.averageRating
--- FROM movies m 
--- JOIN ratings r ON m.tconst = r.tconst
--- JOIN movie_sentiments ms ON m.primaryTitle_norm = ms.name_norm
--- WHERE ms.genre IN ('Action', 'Comedy');
-
--- SELECT 
---     CASE 
---         WHEN r.averageRating >= 7 THEN 'High'
---         WHEN r.averageRating <= 4 THEN 'Low'
---     END AS rating_group,
---     ms.favorability,
---     r.averageRating
--- FROM basics b
--- JOIN ratings r ON b.tconst = r.tconst
--- JOIN movie_sentiments ms ON b.primaryTitle_norm = ms.name_norm
--- WHERE r.averageRating >= 7 OR r.averageRating <= 4
--- ORDER BY rating_group
-
--- SELECT 
---     ms.rating,
---     CASE 
---         WHEN ms.rating = 'R' THEN 'R-Rated'
---         ELSE 'Non R-Rated'
---     END AS rating_group,
---     r.averageRating,
---     ms.favorability
--- FROM basics b
--- JOIN ratings r ON b.tconst = r.tconst
--- JOIN movie_sentiments ms ON b.primaryTitle_norm = ms.name_norm
--- WHERE ms.rating IN ('R', 'PG', 'PG-13', 'G')
--- ORDER BY rating_group, r.averageRating
+SELECT 
+    ms.rating,
+    CASE 
+        WHEN ms.rating = 'R' THEN 'R-Rated'
+        ELSE 'Non R-Rated'
+    END AS rating_group,
+    r.averageRating,
+    ms.favorability
+FROM movies m
+JOIN ratings r ON m.tconst = r.tconst
+JOIN movie_sentiments ms ON m.primaryTitle_norm = ms.name_norm
+WHERE ms.rating IN ('R', 'PG', 'PG-13', 'G')
+ORDER BY rating_group, r.averageRating;
